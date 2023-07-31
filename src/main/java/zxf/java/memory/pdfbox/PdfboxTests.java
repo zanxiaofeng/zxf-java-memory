07019c1e2cbd8dbe42c35e2e079cb2e87e4754d0
davis <zanxiaofeng@163.com>
2023-07-31 19:45:12 +0800

    update

--- src/main/java/zxf/java/memory/pdfbox/PdfboxTests.java
+++ src/main/java/zxf/java/memory/pdfbox/PdfboxTests.java
@@ -9,12 +9,15 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PdfboxTests {
     public static void main(String[] args) throws IOException, InterruptedException {
+        waitInputFromKeyboard("Please press any key for start");
         loadPdf();
         DebugUtils.printMemInfoFromRuntime("main.1");
-//        System.gc();
+        System.gc();
+        waitInputFromKeyboard("Please press any key for end");
 //        DebugUtils.printMemInfoFromRuntime("main.2");
 //        loadPdf();
 //        DebugUtils.printMemInfoFromRuntime("main.3");
@@ -31,16 +34,24 @@ public class PdfboxTests {
         DebugUtils.printMemInfoFromRuntime("loadPdf.start");
         //DebugUtils.callJmap("loadPdf.start");
         List<BufferedImage> results = new ArrayList<>();
-        try (PDDocument pdfDocument = PDDocument.load(Paths.get("./testpdf/developer-mozilla-org-CORS-en.pdf").toFile())) {
+        try (PDDocument pdfDocument = PDDocument.load(Paths.get("./test-pdf/developer-mozilla-org-CORS-en.pdf").toFile())) {
             DebugUtils.printMemInfoFromRuntime("loadPdf.after.load");
             //DebugUtils.callJmap("loadPdf.after.load");
             PDFRenderer pdfRenderer = new PDFRenderer(pdfDocument);
             for (int pageIndex = 0; pageIndex < pdfDocument.getNumberOfPages(); pageIndex++){
                 BufferedImage image = pdfRenderer.renderImageWithDPI(pageIndex, 200);
                 results.add(image);
+                waitInputFromKeyboard("Please press any key for loadPdf.each");
             }
         }
         DebugUtils.printMemInfoFromRuntime("loadPdf.end");
+        waitInputFromKeyboard("Please press any key for loadPdf.end");
         //DebugUtils.callJmap("loadPdf.end");
+    }
+
+    private static void waitInputFromKeyboard(String prompt){
+        Scanner keyboardScanner = new Scanner(System.in);
+        System.out.println(prompt + ":\n");
+        keyboardScanner.next();
     }
 }
