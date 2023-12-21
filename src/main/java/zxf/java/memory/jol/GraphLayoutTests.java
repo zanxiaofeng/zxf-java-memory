e09f2337a0b8d266898bf06d847f5932b756400b
davis <zanxiaofeng@163.com>
2023-12-21 09:16:07 +0800

    update

--- src/main/java/zxf/java/memory/jol/GraphLayoutTests.java
+++ src/main/java/zxf/java/memory/jol/GraphLayoutTests.java
@@ -6,12 +6,12 @@ import java.util.Arrays;
 
 public class GraphLayoutTests {
     public static void main(String[] args) {
-        Foo foo1 = Foo.create(Arrays.asList("12345", "67890", "ABCDE", "54321", "09876"));
+        Foo foo1 = Foo.create(Arrays.asList("12345A", "67890", "ABCDE", "54321", "09876"));
         System.out.println(GraphLayout.parseInstance(foo1).toPrintable());
         System.out.println(GraphLayout.parseInstance(foo1).toFootprint());
         System.out.println("totalSize: " + GraphLayout.parseInstance(foo1).totalSize());
 
-        Foo foo2 = Foo.create(Arrays.asList("12345", "67890", "ABCDE", "54321", "09876", "12345", "67890", "ABCDE", "54321", "09876"));
+        Foo foo2 = Foo.create(Arrays.asList("12345A", "67890", "ABCDE", "54321", "09876", "12345", "67890", "ABCDE", "54321", "09876"));
         System.out.println(GraphLayout.parseInstance(foo2).toPrintable());
         System.out.println(GraphLayout.parseInstance(foo2).toFootprint());
         System.out.println("totalSize: " + GraphLayout.parseInstance(foo2).totalSize());
