ba8e5c2e45f264c819406fc0be30b7344a27414d
davis <zanxiaofeng@163.com>
2023-12-21 09:34:05 +0800

    update

--- src/main/java/zxf/java/memory/jol/ClassLayoutTests.java
+++ src/main/java/zxf/java/memory/jol/ClassLayoutTests.java
@@ -3,6 +3,8 @@ package zxf.java.memory.jol;
 import org.openjdk.jol.info.ClassLayout;
 import org.openjdk.jol.vm.VM;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class ClassLayoutTests {
@@ -11,7 +13,7 @@ public class ClassLayoutTests {
         System.out.println("#################Class Info#################");
         System.out.println(ClassLayout.parseClass(Foo.class).toPrintable());
         System.out.println(ClassLayout.parseClass(Foo.Bar.class).toPrintable());
-        System.out.println("#################Instance Info#################");
+        System.out.println("#################Instance Info of Foo#################");
         Foo foo = Foo.create(Arrays.asList("12345", "67890"));
         System.out.println(ClassLayout.parseInstance(foo).toPrintable());
         System.out.println(ClassLayout.parseInstance(foo.getStr().toCharArray()).toPrintable());
@@ -20,5 +22,10 @@ public class ClassLayoutTests {
         System.out.println(ClassLayout.parseInstance(foo.getBar().getIds()).toPrintable());
         System.out.println(ClassLayout.parseInstance(foo.getBar().getIds().get(0)).toPrintable());
         System.out.println(ClassLayout.parseInstance(foo.getBar().getIds().get(1)).toPrintable());
+        System.out.println("#################Instance Info of String#################");
+        System.out.println(ClassLayout.parseInstance("1235赞").toPrintable());
+
+        System.out.println("#################Instance Info of Byte[]#################");
+        System.out.println(ClassLayout.parseInstance("1235赞".getBytes(StandardCharsets.UTF_16LE)).toPrintable());
     }
 }
--- src/main/java/zxf/java/memory/jol/GraphLayoutTests.java
+++ src/main/java/zxf/java/memory/jol/GraphLayoutTests.java
@@ -2,15 +2,30 @@ package zxf.java.memory.jol;
 
 import org.openjdk.jol.info.GraphLayout;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class GraphLayoutTests {
     public static void main(String[] args) {
+        System.out.println("#################GraphLayout Info of String#################");
+        String str = "1235赞";
+        System.out.println(GraphLayout.parseInstance(str).toPrintable());
+        System.out.println(GraphLayout.parseInstance(str).toFootprint());
+        System.out.println("totalSize: " + GraphLayout.parseInstance(str).totalSize());
+
+        System.out.println("#################GraphLayout Info of Byte[]#################");
+        byte[] bytes = "1235赞".getBytes(StandardCharsets.UTF_16LE);
+        System.out.println(GraphLayout.parseInstance(bytes).toPrintable());
+        System.out.println(GraphLayout.parseInstance(bytes).toFootprint());
+        System.out.println("totalSize: " + GraphLayout.parseInstance(bytes).totalSize());
+
+        System.out.println("#################GraphLayout Info of Foo1#################");
         Foo foo1 = Foo.create(Arrays.asList("12345A", "67890", "ABCDE", "54321", "09876"));
         System.out.println(GraphLayout.parseInstance(foo1).toPrintable());
         System.out.println(GraphLayout.parseInstance(foo1).toFootprint());
         System.out.println("totalSize: " + GraphLayout.parseInstance(foo1).totalSize());
 
+        System.out.println("#################GraphLayout Info of Foo2#################");
         Foo foo2 = Foo.create(Arrays.asList("12345A", "67890", "ABCDE", "54321", "09876", "12345", "67890", "ABCDE", "54321", "09876"));
         System.out.println(GraphLayout.parseInstance(foo2).toPrintable());
         System.out.println(GraphLayout.parseInstance(foo2).toFootprint());
