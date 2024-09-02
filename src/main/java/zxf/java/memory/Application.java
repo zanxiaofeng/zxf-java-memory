dd5955a6f50ac01a73e94f9a676d41aacba6b1dd
Davis ZAN <zanxiaofeng@163.com>
2024-09-02 18:56:40 -0500

    update

--- src/main/java/zxf/java/memory/Application.java
+++ src/main/java/zxf/java/memory/Application.java
@@ -2,10 +2,15 @@ package zxf.java.memory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            System.out.println("Exception, thread=" + thread + ", throwable=" + throwable);
+            throwable.printStackTrace(System.out);
+        });
         SpringApplication.run(Application.class, args);
     }
 }
