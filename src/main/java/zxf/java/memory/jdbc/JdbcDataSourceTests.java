4c46dfd0c383432363d2af3bc3fa26a9fcc0bdb3
Davis ZAN <zanxiaofeng@163.com>
2024-10-20 10:39:25 +0800

    update

--- src/main/java/zxf/java/memory/jdbc/JdbcDataSourceTests.java
+++ src/main/java/zxf/java/memory/jdbc/JdbcDataSourceTests.java
@@ -5,30 +5,63 @@ import oracle.ucp.jdbc.PoolDataSourceFactory;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcDataSourceTests {
     public static void main(String[] args) throws SQLException {
-
+        testOracleDataSource();
 
     }
 
     private static void testOracleDataSource() throws SQLException {
         OracleDataSource dataSource = new OracleDataSource();
+        dataSource.setDriverType("oracle.jdbc.OracleDriver");
         dataSource.setURL("jdbc:log4jdbc:oracle:thin:@host:port/service");
         dataSource.setUser("***");
         dataSource.setPassword("***");
         System.out.println("DataSource->" + dataSource.getClass());
+
         Connection connection = dataSource.getConnection();
         System.out.println("Connection->" + connection.getClass());
+
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT DBTIMEZONE, SESSIONTIMEZONE FROM DUAL");
+        System.out.println("PreparedStatement->" + preparedStatement.getClass());
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        System.out.println("ResultSet->" + resultSet.getClass());
+
+        if (resultSet.next()) {
+            String dbTimezone = resultSet.getString("DBTIMEZONE");
+            System.out.println("DBTIMEZONE => " + dbTimezone.toString());
+            String sessionTimezone = resultSet.getString("SESSIONTIMEZONE");
+            System.out.println("SESSIONTIMEZONE => " + sessionTimezone.toString());
+        }
+
         connection.close();
     }
 
     private static void testPoolOracleDataSource() throws SQLException {
         DataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
         System.out.println("DataSource->" + dataSource.getClass());
+
         Connection connection = dataSource.getConnection();
         System.out.println("Connection->" + connection.getClass());
+
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT DBTIMEZONE, SESSIONTIMEZONE FROM DUAL");
+        System.out.println("PreparedStatement->" + preparedStatement.getClass());
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        System.out.println("ResultSet->" + resultSet.getClass());
+
+        if (resultSet.next()) {
+            String dbTimezone = resultSet.getString("DBTIMEZONE");
+            System.out.println("DBTIMEZONE => " + dbTimezone.toString());
+            String sessionTimezone = resultSet.getString("SESSIONTIMEZONE");
+            System.out.println("SESSIONTIMEZONE => " + sessionTimezone.toString());
+        }
+
         connection.close();
     }
 }
